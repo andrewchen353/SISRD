5a9b4beebc7960ba47ec437d154c86364ed0e007
Andrew Chen <chen1458@purdue.edu>
2018-12-02 00:54:01 -0600

    Implemented one version of neural network, needs testing

--- Andrew/implementation1/main.py
+++ Andrew/implementation1/main.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import cv2
 import neural_net
-from PIL import Image
+import sys
 
 
 def load_images(path):
@@ -12,10 +12,37 @@ def load_images(path):
         img.append(cv2.bitwise_not(im)/255)
     return np.array(img)
 
+def save_images(path, imageNames, images):
+    names = sorted(listdir(imageNames))
+    for file, i in zip(names, np.arange(len(names))):
+        img = images[i]
+        w, h, _ = img.shape
+        img.resize((w,h))
+        img = img * 255
+        img = img.astype(np.int)
+        imwrite(path + file, img)
+
+    return
+
 def main():
-    train_64_path = "train_images_64x64/"
-    train_128_path = "train_images_128x128/"
-    images_64 = load_images(train_64_path)
-    images_128 = load_images(train_128_path)
+    test_64_path = "xray/test_images_64x64/"
+    train_64_path = "xray/train_images_64x64/"
+    train_128_path = "xray/train_images_128x128/"
+
+    if len(sys.argv) != 4:
+        print("Usage: main.py <--train/--test> --model <model_name>")
+        exit(1)
+        
+    if sys.argv[1] == "--train":
+        nn = createModel()
+        train_images_64 = load_images(train_64_path)
+        train_images_128 = load_images(train_128_path)
+        nn.fit(train_images_64, train_images_128, batch_size=1000, epochs=50)
+        model.save(sys.argv[3])
+    elif sys.argv[1] == "--test":
+        nn = model.loadModel(argv[3])
+        test_images_64 = load_images(test_64_path)
+        test_out_128 = nn.predict(test_images_64)
+        save_images("xray/test_images_128x128", test_images_64, test_out_128)
 
 main()
--- Andrew/implementation1/neural_net.py
+++ Andrew/implementation1/neural_net.py
@@ -1,5 +1,42 @@
 import numpy as np
 from keras.layers import Input, MaxPooling2D, UpSampling2D
+from keras.models import Sequential, load_model
 
-def nn():
-    nn_input = Input((64, 64, 1))
+######################################################
+# By default keras is using TensorFlow as a backend
+######################################################
+
+######################################################
+# Implementing model in
+# "Medical image denoising using convolutional denoising autoencoders"
+######################################################
+
+def createModel():
+    # nn_input = Input((1, 64, 64))
+    model = Sequential()
+    
+    model.add(Conv2D(64, (2, 2), activation='relu'))
+    model.add(MaxPooling2D((2, 2), (2, 2)))
+
+    model.add(Conv2D(64, (2, 2), activation='relu'))
+    model.add(MaxPooling2D((2, 2), (2, 2)))
+
+    model.add(Conv2D(64, (2, 2), activation='relu'))
+    model.add(UpSampling2D(size=(2, 2)))
+
+    model.add(Conv2D(64, (2, 2), activation='relu'))
+    model.add(UpSampling2D(size=(2, 2)))
+
+    model.add(Conv2D(1, (2, 2), activation='relu'))
+
+    model.add(Flatten())
+    model.add(Dense(64*64, activation='softmax'))
+    model.compile(loss='rmse', optimizer=Adam(lr=0.03), metrics=['accuracy'])
+
+    return model
+
+def loadModel(model):
+    return load_model(model)
+
+def saveModel(model, name):
+    model.save(name)
