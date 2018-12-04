96e8c7212f04b70ef527a9fbff64224c0d89463f
Andrew Chen <andrewchen686@gmail.com>
2018-12-04 12:44:25 -0600

    Updated model, needs testing

--- Andrew/implementation1/main.py
+++ Andrew/implementation1/main.py
@@ -6,11 +6,22 @@ import sys
 
 
 def load_images(path):
-    img = []
-    for p in sorted(os.listdir(path)):
-        im = cv2.imread(path + p, 0)
-        img.append(cv2.bitwise_not(im)/255)
-    return np.array(img)
+    # img = []
+    # for p in sorted(os.listdir(path)):
+    #     im = cv2.imread(path + p, 0)
+    #     w, h = im.shape
+    #     im.resize((w, h, 1))
+    #     img.append(cv2.bitwise_not(im)/255)
+    # return np.array(img)
+    data = []
+    for file in sorted(os.listdir(path)):
+        img = cv2.imread(path + file, 0)
+        img = cv2.resize(img, (128,128), interpolation=cv2.INTER_CUBIC)
+        w, h = img.shape
+        img.resize((w,h,1))
+        img = img.astype(np.float32) / 255
+        data.append(img)
+    return np.array(data)
 
 def save_images(path, imageNames, images):
     names = sorted(listdir(imageNames))
@@ -19,7 +30,7 @@ def save_images(path, imageNames, images):
         w, h, _ = img.shape
         img.resize((w,h))
         img = img * 255
-        img = img.astype(np.int)
+        img = img.astype(np.uint8)
         imwrite(path + file, img)
 
 def main():
@@ -35,18 +46,19 @@ def main():
         print("Creating model...")
         nn = neural_net.createModel()
         print("Loading images...")
-        train_images_64 = load_images(train_64_path)
-        print(train_images_64.shape)
-        train_images_128 = load_images(train_128_path)
+        train_input = load_images(train_64_path)
+        print(train_input.shape)
+        train_output = load_images(train_128_path)
+        print(train_output.shape)
         print("Training model...")
-        nn.fit(train_images_64, train_images_128, batch_size=4000, epochs=50)
+        nn.fit(train_input, train_output, batch_size=4000, epochs=50)
         print("Saving model")
         model.save(sys.argv[3])
     elif sys.argv[1] == "--test":
         nn = load_model(sys.argv[3]) # model.loadModel(argv[3])
         test_images_64 = load_images(test_64_path)
         test_out_64 = nn.predict(test_images_64)
-        test_out_128 = [cv2.resize(img, (128,128), interpolation=INTER_CUBIC) for img in test_out_64]
+        # test_out_128 = [cv2.resize(img, (128,128), interpolation=INTER_CUBIC) for img in test_out_64]
         save_images("xray/test_images_128x128", test_images_64, test_out_128)
 
 main()
--- Andrew/implementation1/neural_net.py
+++ Andrew/implementation1/neural_net.py
@@ -1,7 +1,7 @@
 import numpy as np
 from keras.layers import Input, MaxPooling2D, UpSampling2D, Conv2D, Flatten, Dense
 from keras.optimizers import Adam
-from keras.models import Sequential, load_model
+from keras.models import Model, Sequential, load_model
 
 ######################################################
 # By default keras is using TensorFlow as a backend
@@ -13,24 +13,35 @@ from keras.models import Sequential, load_model
 ######################################################
 
 def createModel():
-    model = Sequential() #input_shape=(1, 64, 64))
+    # model = Sequential() #input_shape=(1, 64, 64))
     
-    model.add(Conv2D(64, (2, 2), padding='same', activation='relu', input_shape=(1, 64, 64)))
-    model.add(MaxPooling2D((2, 2), (2, 2)))
+    # model.add(Conv2D(64, (2, 2), padding='same', activation='relu', input_shape=(1, 64, 64)))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(64, (2, 2), padding='same', activation='relu'))
-    model.add(MaxPooling2D((2, 2), (2, 2)))
+    # model.add(Conv2D(64, (2, 2), padding='same', activation='relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))#, strides=(2, 2)))
 
-    model.add(Conv2D(64, (2, 2), padding='same', activation='relu'))
-    model.add(UpSampling2D(size=(2, 2)))
+    # model.add(Conv2D(64, (2, 2), padding='same', activation='relu'))
+    # model.add(UpSampling2D(size=(2, 2)))
 
-    model.add(Conv2D(64, (2, 2), padding='same', activation='relu'))
-    model.add(UpSampling2D(size=(2, 2)))
+    # model.add(Conv2D(64, (2, 2), padding='same', activation='relu'))
+    # model.add(UpSampling2D(size=(2, 2)))
 
-    model.add(Conv2D(1, (2, 2), padding='same', activation='relu'))
+    # model.add(Conv2D(1, (2, 2), padding='same', activation='relu'))
 
-    # model.add(Flatten())
-    # model.add(Dense(64*64, activation='relu'))
+    x_input = Input((128, 128, 1))
+    conv1 = Conv2D(64, (2, 2), padding='same', activation='relu')(x_input)
+    mp1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(64, (2, 2), padding='same', activation='relu')(mp1)
+    mp2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(64, (2, 2), padding='same', activation='relu')(mp2)
+    up1 = UpSampling2D(size=(2, 2))(conv3)
+    conv4 = Conv2D(64, (2, 2), padding='same', activation='relu')(up1)
+    up2 = UpSampling2D(size=(2, 2))(conv4)
+    conv5 = Conv2D(1, (2, 2), padding='same', activation='relu')(up2)
+
+    model = Model(x_input, conv5)
+
     model.compile(loss='mean_squared_error', optimizer=Adam(lr=0.03), metrics=['accuracy'])
 
     return model
