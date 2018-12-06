dabee7ff0285b314aed007aac247e4441e47dca9
Patrick Cole <cole106@purdue.edu>
2018-12-06 00:31:26 -0600

    Adding implementation that uses subpixel

---
+++ Patrick/implementation_3/model.py
@@ -0,0 +1,48 @@
+import numpy as np
+from keras.layers import Input, Add, Conv2D, Deconv2D, UpSampling2D, BatchNormalization, LeakyReLU
+from keras.activations import relu
+from keras.models import Model, load_model
+from keras.optimizers import Adam
+from keras.callbacks import Callback
+from keras import backend as K
+import h5py
+
+from subpixel import SubpixelConv2D
+
+_EPSILON = K.epsilon()
+_W = 128
+_H = 128
+
+######################################################
+# By default keras is using TensorFlow as a backend
+######################################################
+
+def generate_model():
+    x_input = Input((64, 64, 1))
+
+    conv1 = Conv2D(64, (5,5), padding='same', use_bias=True, activation='relu', name='conv1')(x_input)
+    conv2 = Conv2D(32, (3,3), padding='same', use_bias=True, activation='relu', name='conv2')(conv1)
+    conv3 = Conv2D(4 , (3,3), padding='same', use_bias=True, activation='relu', name='conv3')(conv2)
+    subpix = SubpixelConv2D(conv3.shape, scale=2)(conv3)
+
+    y_output = subpix
+
+    model = Model(x_input, y_output)
+    adam = Adam(lr=0.001)
+    model.compile(optimizer=adam, loss=rmse, metrics=['accuracy'])
+    return model
+
+def rmse(y_true, y_pred):
+    diff = K.square(y_pred - y_true)
+    return K.sum(K.sqrt(K.sum(K.sum(diff, axis=1), axis=2) / (_W * _H)))
+
+def loadModel(modelName):
+    return load_model(modelName, custom_objects={'rmse': rmse})
+    #return load_model(modelName)
+
+def saveModel(model, modelName):
+    model.save(modelName)
+
+if __name__ == "__main__":
+    print('test model')
+    generate_model()
