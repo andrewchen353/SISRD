e032fac4d3c2f5eba8d53786c41a8061a62faf2f
Andrew Chen <andrewchen686@gmail.com>
2018-12-08 12:45:50 -0600

    Updated DSRCNN to use Average instead of Add for merging layers

--- Andrew/neural_net.py
+++ Andrew/neural_net.py
@@ -1,7 +1,7 @@
 import numpy as np
 import loss
 from subpixel import SubpixelConv2D
-from keras.layers import Add, Input, Conv2D, Deconv2D, MaxPooling2D, UpSampling2D, PReLU, BatchNormalization
+from keras.layers import Add, Average, Input, Conv2D, Deconv2D, MaxPooling2D, UpSampling2D, PReLU, BatchNormalization
 from keras.optimizers import Adam
 from keras.models import Model, load_model
 
@@ -43,15 +43,20 @@ def CNNDAE():
 def DSRCNN():
     x_input = Input((64, 64, 1))
 
-    conv1   = Conv2D(64, (3, 3), padding='same', use_bias=True, activation='relu')(x_input)
-    conv2   = Conv2D(64, (3, 3), padding='same', use_bias=True, activation='relu')(conv1)
+    # conv1   = Conv2D(64, (3, 3), padding='same', use_bias=True, activation='relu')(x_input) #v1-2
+    conv1   = Conv2D  (64, (5, 5), padding='same', use_bias=True, activation='relu')(x_input)
+    conv2   = Conv2D  (64, (5, 5), padding='same', use_bias=True, activation='relu')(conv1)
     deconv1 = Deconv2D(64, (3, 3), padding='same', use_bias=True)(conv2)
     
-    add1    = Add()([conv2, deconv1])
-    deconv2 = Deconv2D(64, (3, 3), padding='same', use_bias=True)(add1)
+    # add1    = Add()([conv2, deconv1])
+    # deconv2 = Deconv2D(64, (3, 3), padding='same', use_bias=True)(add1) #v1-2
+    avg1    = Average()([conv2, deconv1])
+    deconv2 = Deconv2D(64, (3, 3), padding='same', use_bias=True)(avg1) #v3
     
-    add2  = Add()([conv1, deconv2])
-    conv3 = Conv2D(4, (3, 3), padding='same', use_bias=True, activation='relu')(add2)
+    # add2  = Add()([conv1, deconv2])
+    # conv3 = Conv2D(4, (3, 3), padding='same', use_bias=True, activation='relu')(add2) #v1-2
+    avg2  = Average()([conv1, deconv2])
+    conv3 = Conv2D(4, (3, 3), padding='same', use_bias=True, activation='relu')(avg2) #v3
     spc1  = SubpixelConv2D(conv3.shape, scale=2)(conv3)
 
     model = Model(x_input, spc1)
