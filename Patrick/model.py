0bb79c0bf0458660d5e8a3ec8cbff905782acf91
Patrick Cole <cole106@purdue.edu>
2018-12-08 19:13:42 -0600

    adding patrick test model

--- Patrick/model.py
+++ Patrick/model.py
@@ -105,6 +105,34 @@ def dsrcnn(learningRate=0.001):
     model.compile(optimizer=adam, loss=rmse, metrics=['accuracy'])
     return model
 
+#########################################################
+# test
+#########################################################
+
+def testnet(learningRate=0.001):
+    print('Creating model of architecture \'testnet\'')
+    x_input = Input((64, 64, 1))
+
+    conv1 = Conv2D(64, (5,5), padding='same', use_bias=True, activation='relu', name='conv1')(x_input)
+    conv2 = Conv2D(64, (3,3), padding='same', use_bias=True, activation='relu', name='conv2')(conv1)
+
+    deconv = Deconv2D(64, (3,3), padding='same', use_bias=True, activation='relu', name='deconv')(conv2)
+    add1 = Add()([conv2, deconv])
+
+    deconv2 = Deconv2D(64, (3,3), padding='same', use_bias=True, activation='relu', name='deconv2')(add1)
+    add2 = Add()([deconv2, conv1])
+
+    conv3 = Conv2D(32 , (3,3), padding='same', use_bias=True, activation='relu', name='conv3')(add2)
+    subpix = SubpixelConv2D(conv3.shape, scale=2, name='subpix1')(conv3)
+    conv4 = Conv2D(1 , (3,3), padding='same', use_bias=True, activation='relu', name='conv4')(subpix)
+
+    y_output = conv4
+
+    model = Model(x_input, y_output)
+    adam = Adam(lr=learningRate)
+    model.compile(optimizer=adam, loss=rmse, metrics=['accuracy'])
+    return model
+
 def rmse(y_true, y_pred):
     diff = K.square(255 * (y_pred - y_true))
     return K.sum(K.sqrt(K.sum(diff, axis=(2,1)) / (_W * _H)))
@@ -125,6 +153,7 @@ lookUp['srcnn'] = srcnn
 lookUp['subpixel'] = subpixelsrcnn
 lookUp['esrcnn'] = esrcnn
 lookUp['dsrcnn'] = dsrcnn
+lookUp['test'] = testnet
 
 if __name__ == "__main__":
     print('test model')
@@ -132,3 +161,4 @@ if __name__ == "__main__":
     lookUp['subpixel'](0.003) #.summary()
     lookUp['esrcnn'](0.003) #.summary()
     lookUp['dsrcnn'](0.003) #.summary()
+    lookUp['test'](0.003).summary()
